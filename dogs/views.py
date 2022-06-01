b99a7b8c9484a11309bdeec62c8c4f0d470bde1f
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-31 23:06:07 -0500

    Added Django app

---
+++ dogs/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import Dog
+from .serializers import DogSerializer
+
+
+class DogList(generics.ListCreateAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+
+class DogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
